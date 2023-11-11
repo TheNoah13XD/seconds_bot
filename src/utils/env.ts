2254c9ca2152a92b69148954c28e77ec470de222
TheNoah13XD <broken.personal.1211@gmail.com>
2023-11-11 21:44:20 +0530

    Initial commit

---
+++ src/utils/env.ts
@@ -0,0 +1,19 @@
+import { config } from "dotenv";
+import { resolve } from "path";
+
+const EnvFile = process.env.NODE_ENV === "development" 
+  ? ".env.local" 
+  : ".env";
+
+const EnvFilePath = resolve(process.cwd(), EnvFile);
+
+config({ path: EnvFilePath });
+
+export function getEnvVar(name: string, fallback?: string): string {
+    const value = process.env[name] ?? fallback;
+    if (value === undefined) {
+        throw new Error(`Environment variable ${name} is not defined.`);
+    }
+
+    return value;
+}
